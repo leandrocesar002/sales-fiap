01f943b32e4f4a0e9d0ef0395193a7f3decd32f2
leandrocesar001 <leandro.santos@fintalk.ai>
2025-02-13 19:33:58 -0300

    create docker

--- src/application/cadastrarPagamento.js
+++ src/application/cadastrarPagamento.js
@@ -1,7 +1,7 @@
 const Pagamento = require("../infrastructure/database/models/PagamentoModel");
 const axios = require("axios");
 
-const VENDAS_SERVICE_URL = "http://localhost:4000/vendas";
+const VENDAS_SERVICE_URL = "http://54.211.144.92:4000/vendas";
 const VEICULO_SERVICE_URL = "http://54.211.144.92:3000/api/veiculos";
 
 const cadastrarPagamento = async (venda_id, codigo_pagamento, status_pagamento) => {
@@ -10,21 +10,18 @@ const cadastrarPagamento = async (venda_id, codigo_pagamento, status_pagamento) 
             throw new Error("Todos os campos são obrigatórios!");
         }
 
-        // Cadastra o pagamento no banco
         const pagamento = await Pagamento.create({
             venda_id,
             codigo_pagamento,
             status_pagamento,
         });
 
-        // Se o pagamento for aprovado, busca o veiculo_id da venda antes de atualizar o veículo
         if (status_pagamento.toUpperCase() === "APROVADO") {
             try {
                 console.log("🔍 Buscando todas as vendas...");
                 const vendaResponse = await axios.get(`${VENDAS_SERVICE_URL}`);
                 console.log("📦 Lista de vendas recebida:", vendaResponse.data);
 
-                // Filtra a venda correta pelo ID
                 const vendaEncontrada = vendaResponse.data.find(venda => venda.id === venda_id);
 
                 if (!vendaEncontrada) {
@@ -39,7 +36,6 @@ const cadastrarPagamento = async (venda_id, codigo_pagamento, status_pagamento) 
                     return pagamento;
                 }
 
-                // Atualiza o status do veículo para "VENDIDO"
                 await axios.put(`${VEICULO_SERVICE_URL}/${veiculo_id}`, { status: "VENDIDO" });
                 console.log(`✅ Veículo ${veiculo_id} atualizado para VENDIDO.`);
 
